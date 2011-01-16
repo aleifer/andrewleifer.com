7d912593432dc6f4c7f9cb6d7616fd268cca4642
Andrew Leifer <andrew.leifer@gmail.com>
2011-01-16 16:10:41 -0500

    decomped header and footer

--- index.php
+++ index.php
@@ -1,69 +1,29 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-
-<html lang="en">
-  <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <title>Andrew Leifer</title>
-
-    <!-- Framework CSS -->
-    <link rel="stylesheet" href="css/blueprint/screen.css" type="text/css" media="screen, projection">
-    <link rel="stylesheet" href="css/blueprint/print.css" type="text/css" media="print">
-
-    <!--[if lt IE 8]><link rel="stylesheet" href="css/blueprint/ie.css" type="text/css" media="screen, projection"><![endif]-->
-
-    <!-- Import fancy-type plugin for the sample page. -->
-    <link rel="stylesheet" href="css/blueprint/plugins/fancy-type/screen.css" type="text/css" media="screen, projection">
-
-    <link rel="stylesheet" href="css/style.css" type="text/css" media="screen, projection">
-  </head>
-<body>
-
-<hr id="topstripe">
-<div class="container">
-<h1 id="mast" class="span-8"><a href="/">Andrew Leifer</a></h1>
-
-<div class="span-16 last">
-<ul class="buttonbar">
-<li class="span-2"><a href="/">Home</a></li>
-<li class="span-2"><a href="research.html">Research</a></li>
-<li class="span-2"><a href="writings.html">Writings</a></li>
-<li class="span-2"><a href="speeches.html">Speeches</a></li>
-<li class="span-2"><a href="links.html">Links</a></li>
-<li class="span-2"><a href="software.html">Software</a></li>
-<li class="span-2"><a href="cv.html">CV</a></li>
-</ul>
-</div>
-
-<hr>
-<hr class="space">
-
+<?php
+$title="Home";
+include("header.inc");
+?>
 <div class="span-16 append-1">
-<h2>Bio</h2>
+<h2>Welcome</h2>
 
 My name is Andrew Leifer and I graduated from <a href="http://www.stanford.edu">Stanford University</a> with degrees in Physics, Political Science and honors in International Security Studies.  I am currently a fourth year PhD candidate in the Biophysics program at <a href="http://www.harvard.edu">Harvard University</a>. I conduct behavioral neuorscience research in the <a href="http://worms.physics.harvard.edu">Samuel Lab</a> where I develop new optogenetic techniques to probe the neurocircuitry of the brain. Most everyone calls me Andy. 
 
 This website serves as a repository for my research,  writings and works. Feel free to contact me at:
 <a href="mailto:leifer@fas.harvard.edu">leifer@fas.harvard.edu</a>
+
+<hr class="space">
+<h2>News</h2>
+My work on the <a href="http://colbert.physics.harvard.edu">CoLBeRT</a> system, an instrument to control neurons of a worm as it moves, got published today in <i>Nature Methods</i>. It got a bit of press at <a href="http://www.sciencenews.org/view/generic/id/68860/description/Making_a_worm_do_more_than_squirm" target="_blank"><i>Science News</i></a> and <a href="http://www.scientificamerican.com/article.cfm?id=single-worm-neurons" target="_blank">Scientific American</i></a>.
+
 </div>
+
+
+
 <div class="span-7 last">
 <div class="box">
 <img src="images/fbook_pic.png" width="222">
 </div>
 </div>
 
-<hr class="bigspace">
-<hr/>
-
-<div class="small quiet bottom append-bottom incr span-24 last">
-<span id="cc"><a class="cc"rel="license"  href="http://creativecommons.org/licenses/by-sa/3.0/us/">cc</a></span> 2011 | <a class="cc" href="mailto:leifer@fas.harvard.edu">Andrew Leifer</a>
-<div class="right">Released under  <a class="cc"rel="license"  href="http://creativecommons.org/licenses/by-sa/3.0/us/">Creative Commons Attribution-ShareAlike Licence</a>
-</div>
-</div>
-</div>
-
-
-</div>
-
-
-</body>
-</html>
+<?php
+include("footer.inc");
+?>
---
+++ writings.php
@@ -0,0 +1,14 @@
+<?php
+$title="Writings";
+include("header.inc");
+?>
+<div class="span-16 append-1">
+<h2>Coming Soon</h2>
+
+</div>
+
+
+
+<?php
+include("footer.inc");
+?>
