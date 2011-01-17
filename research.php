ab38308a166d3df1bbd4b1efcb00989d4ddbe660
Andrew Leifer <andrew.leifer@gmail.com>
2011-01-17 09:36:03 -0500

    got files in order

---
+++ research.php
@@ -0,0 +1,31 @@
+<?php
+$title="Research";
+include("header.inc");
+?>
+
+<div class="span-16 append-1">
+<h2>Coming Soon</h2>
+
+
+
+
+</div>
+<div class="span-7 last">
+<a href="http://colbert.physics.harvard.edu">
+<div class="box">
+<img src="images/Diagram.png" width="225">
+</div>
+</a>
+
+<a href="http://colbert.physics.harvard.edu/images.html">
+<div class="box">
+<img src="images/CoLBeRT4_small.jpg" width="225">
+</div>
+</a>
+
+</div>
+
+
+<?php
+include("footer.inc");
+?>
